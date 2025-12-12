a56bee723eca60272e04b397167081ceadfb2b4b
ManmohanSinghM <manmohansinghmatharoo@gmail.com>
2025-12-12 20:48:06 +0530

    Fix case sensitivity import error

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 // notice the "../" below - this is crucial!
-const { register, login } = require('../controllers/authcontroller');
+const { register, login } = require('../controllers/authController');
 
 router.post('/register', register);
 router.post('/login', login);
